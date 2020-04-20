975907e48bbe0acc2858a5b8267a6a5d6f042e02
s.litvin <litvin.xx@gmail.com>
2020-04-20 16:06:24 +0300

    Ru UI

---
+++ packages/Webkul/Ui/src/Resources/lang/ru/app.php
@@ -0,0 +1,40 @@
+<?php
+return [
+    'datagrid' => [
+        'actions' => 'Ру Действия',
+        'id' => 'Ру Index columns have value greater than zero only',
+
+        'massaction' => [
+            'mass-delete-confirm' => 'Ру Do you really want to delete these selected :resource?',
+            'mass-update-status' => 'Ру Do you really want to update status of these selected :resource?',
+            'delete' => 'Ру Do you really want to perform this action?',
+            'edit' => 'Ру Do you really want to edit this :resource?',
+        ],
+
+        'zero-index' => 'Ру Index columns can have values greater than zero only',
+        'no-records' => 'Ру No Records Found',
+        'filter-fields-missing' => 'Ру Some of the required field is null, please check column, condition and value properly',
+        'click_on_action' => 'Ру Do you really want to perform this action?',
+        'search' => 'Ру Search Here...',
+        'filter' => 'Ру Filter',
+        'column' => 'Ру Select Column',
+        'condition' => 'Ру Select Condition',
+        'contains' => 'Ру Contains',
+        'ncontains' => 'Ру Does not contains',
+        'equals' => 'Ру Is Equals to',
+        'nequals' => 'Ру Is Not equals to',
+        'greater' => 'Ру Greater than',
+        'less' => 'Ру Less than',
+        'greatere' => 'Ру Greater than equals to',
+        'lesse' => 'Ру Less than equals to',
+        'value' => 'Ру Select Value',
+        'true' => 'Ру True / Active',
+        'false' => 'Ру False / Inactive',
+        'between' => 'Ру Is between',
+        'apply' => 'Ру Apply',
+        'items-per-page' => 'Ру Items Per Page',
+        'value-here' => 'Ру Value here',
+        'numeric-value-here' => 'Ру Numeric Value here',
+        'submit' => 'Ру Submit'
+    ]
+];
