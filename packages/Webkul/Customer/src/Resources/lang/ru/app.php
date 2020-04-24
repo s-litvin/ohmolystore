746a3d2cc7ea53f6b59b2abf61f5b3670b39a660
Oksana <o.khrystiuk@gmail.com>
2020-04-24 12:24:17 +0300

    Translation into Russian

--- packages/Webkul/Customer/src/Resources/lang/ru/app.php
+++ packages/Webkul/Customer/src/Resources/lang/ru/app.php
@@ -2,19 +2,19 @@
 
 return [
     'wishlist' => [
-        'success' => 'Item Successfully Added To Wishlist',
-        'failure' => 'Item Cannot Be Added To Wishlist',
-        'already' => 'Item Already Present In Your Wishlist',
-        'removed' => 'Item Successfully Removed From Wishlist',
-        'remove-fail' => 'Item Cannot Be Removed From Wishlist',
-        'empty' => 'You Don\'t Have Any Items In Your Wishlist',
-        'select-options' => 'Need To Select Options Before Adding To Wishlist',
-        'remove-all-success' => 'All The Items From Your Wishlist Have Been Removed',
+        'success' => 'Товар успешно добавлен в список желаний',
+        'failure' => 'Товар не может быть добавлен в список желаний',
+        'already' => 'Товар уже присутствует в вашем списке желаний',
+        'removed' => 'Товар успешно удален из списка желаний',
+        'remove-fail' => 'Товар не может быть удален из списка желаний',
+        'empty' => 'У вас нет товаров в списке желаний',
+        'select-options' => 'Необходимо выбрать параметры перед добавлением в список желаний',
+        'remove-all-success' => 'Все элементы из вашего списка желаний были удалены',
     ],
     'reviews' => [
-        'empty' => 'You have not reviewed any of product yet'
+        'empty' => 'Вы еще не просмотрели ни одного товара'
     ],
     'forget_password' => [
-        'reset_link_sent' => 'We have e-mailed your reset password link.'
+        'reset_link_sent' => 'Мы отправили вам ссылку для сброса пароля по электронной почте'
     ]
 ];
