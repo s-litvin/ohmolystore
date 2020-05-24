a78520907c6d625ed6f28bc3189464036641feb9
s.litvin <litvin.xx@gmail.com>
2020-05-24 18:04:18 +0300

    Footer

--- packages/Webkul/Velocity/src/Resources/views/shop/layouts/footer/copy-right.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/layouts/footer/copy-right.blade.php
@@ -2,8 +2,6 @@
     <span>
         @if (core()->getConfigData('general.content.footer.footer_content'))
             {!! core()->getConfigData('general.content.footer.footer_content') !!}
-        @else
-            {!! trans('admin::app.footer.copy-right') !!}
         @endif
     </span>
 </div>
