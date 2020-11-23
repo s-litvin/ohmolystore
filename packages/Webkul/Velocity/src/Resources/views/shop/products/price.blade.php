b1898c0c1996112359062a8461a79f307df670c4
Oksana <o.khrystiuk@gmail.com>
2020-11-23 23:05:14 +0200

    Test

--- packages/Webkul/Velocity/src/Resources/views/shop/products/price.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/products/price.blade.php
@@ -1,6 +1,6 @@
 {!! view_render_event('bagisto.shop.products.price.before', ['product' => $product]) !!}
 
-<div class="product-price">
+<div class="product-price asdf">
     {!! $product->getTypeInstance()->getPriceHtml() !!}
 </div>
 
